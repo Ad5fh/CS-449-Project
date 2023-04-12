184de2713ee70346f908556673ff23b6975b52c4
Aissatou Diallo <91103577+Ad5fh@users.noreply.github.com>
2023-04-11 23:01:14 -0500

    Add files via upload
    For Sprint 3

---
+++ simple.java
@@ -0,0 +1,24 @@
+package game;
+
+
+public class simple extends board {
+	
+	public simple(int size) {
+		super(size);
+	}
+	
+	public void checkForWin() {
+		int pointsToWin = 1; 
+		int maxPossibleMoves = size * size;
+		
+		if(bluePoints >= pointsToWin)
+			currentGameState = GameState.B_WON;
+		else if (redPoints >= pointsToWin)
+			currentGameState = GameState.R_WON;
+		else if (totalMoves < maxPossibleMoves)
+			currentGameState = GameState.PLAYING;
+		else currentGameState = GameState.DRAW;
+	}
+
+	
+}
